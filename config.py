409985bacd845aad3874b22a623a2da37110fee2
anishmusician <84088428+anishmusician@users.noreply.github.com>
2025-11-27 13:02:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,5 +31,5 @@ class PyroConf(object):
     BOT_START_TIME = time()
 
     MAX_CONCURRENT_DOWNLOADS = int(getenv("MAX_CONCURRENT_DOWNLOADS", "3"))
-    BATCH_SIZE = int(getenv("BATCH_SIZE", "10"))
+    BATCH_SIZE = int(getenv("BATCH_SIZE", "100"))
     FLOOD_WAIT_DELAY = int(getenv("FLOOD_WAIT_DELAY", "3"))
